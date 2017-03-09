c6285e653d858e17e6ab37f6189cbfdff2e78f60
shortmonkey <brilav@gmail.com>
2017-03-09 17:43:04 -0400

    playing with model

--- display_posts/models.py
+++ display_posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    author = models.CharField(max_length=50, blank=False, null=False)
+    post_text = models.TextField(blank=False, null=False)
+    date_posted = models.DateTimeField(blank=True, null=True)
+    is_reviewed = models.BooleanField(default=False, verbose_name="Reviewed?")
+    
+    def __str__(self):
+        return self.author
